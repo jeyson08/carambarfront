d804d18ac6897bd8b2d0aee6ad0b1e3c5d4fc5ef
Jeyson Boursault <jeyson.boursault@gmail.com>
2023-06-19 15:05:36 +0200

    app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,30 @@
-console.log("hello")
+// AFFICHER TOUTES LES BLAGUES POUR TESTER DANS LA CONSOLE
+// fetch("https://blaguecarambar.onrender.com/api/v1/blagues")
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach(joke => {
+//       console.log(joke.blague);
+//     });
+//   })
+//   .catch(error => {
+//     console.error("Une erreur s'est produite:", error);
+//   });
+const button = document.querySelector("button")
+
+function displayRandomJoke() {
+    fetch("https://blaguecarambar.onrender.com/api/v1/blagues")
+      .then(response => response.json())
+      .then(data => {
+        const randomJoke = data[Math.floor(Math.random() * data.length)];
+        const resultDiv = document.getElementById("result");
+        resultDiv.textContent = randomJoke.blague;
+      })
+      .catch(error => {
+        console.error("Une erreur s'est produite :", error);
+      });
+  }
+
+button.addEventListener('click', () => {
+    displayRandomJoke()
+})
+
